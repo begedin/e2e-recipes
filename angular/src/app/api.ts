64a023c51d542c314ec85c7fca3cda6ba4dcd975
begedin <begedinnikola@gmail.com>
2020-08-19 07:17:57 +0200

    Document angular API

--- angular/src/app/api.ts
+++ angular/src/app/api.ts
@@ -9,6 +9,19 @@ const getSandboxCookie = () => {
   return cookie ? cookie[1] : null;
 };
 
+/**
+ * Builds an axios client for usage within the app.
+ *
+ * NOTE: Angular uses protractor as its e2e test runner.
+ *
+ * Protractor does not easily support http request proxying, so there is no
+ * simple way to add request headers to all network requests triggered by
+ * protractor.
+ *
+ * Instead, protractor sets a cookie before every test and the app itself
+ * adds the content of that cookie as a sandbox header, to support sandboxed
+ * tests.
+ */
 const client = () => {
   const token = window.localStorage.getItem('token');
   const sandbox = getSandboxCookie();
