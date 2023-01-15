c2a9d01d65c1d2a9b50bd3635965b171766c7575
Nikola Begedin <begedinnikola@gmail.com>
2023-01-15 17:07:48 +0100

    Expose vite server, use ip via cypress

--- vue/cypress.config.ts
+++ vue/cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173/',
+    baseUrl: 'http://127.0.0.1:5173/',
   },
 });
--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: true,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
