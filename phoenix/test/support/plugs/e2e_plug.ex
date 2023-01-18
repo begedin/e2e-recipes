e14409448e6b8459624f2241467f382a1e71a8ec
Nikola Begedin <begedinnikola@gmail.com>
2023-01-18 21:13:30 +0100

    Update some outdated docs in phoenix project (#68)

--- phoenix/test/support/plugs/e2e_plug.ex
+++ phoenix/test/support/plugs/e2e_plug.ex
@@ -1,11 +1,9 @@
 defmodule E2E.E2EPlug do
   @moduledoc """
-  Exposes endpoints required for effective writing of E2E tests
+  Exposes endpoints which allow the frontend to remotely call factory functions
+  for test setup.
 
-  GET /api/sent_emails renders a JSON of sent emails
-  POST /api/factory can be used to create database records, similar to how it's done in unit tests
 
-  Is effectively a rudimentary interface to `:ex_machina`.
   """
   @behaviour Plug
 
@@ -27,7 +25,7 @@ defmodule E2E.E2EPlug do
   def call(%Conn{method: "POST", request_path: "/api/factory"} = conn, enabled: true) do
     with {:ok, schema} <- Map.fetch(conn.body_params, "schema"),
          {:ok, attrs} <- Map.fetch(conn.body_params, "attributes") do
-      db_schema = String.to_atom(schema)
+      db_schema = String.to_existing_atom(schema)
       db_attrs = atomize(attrs)
 
       rendered =
@@ -35,9 +33,9 @@ defmodule E2E.E2EPlug do
         |> create(db_attrs, Map.get(conn.body_params, "count"))
         |> Jason.encode!()
 
-      Conn.send_resp(conn, 200, rendered) |> Conn.halt()
+      conn |> Conn.send_resp(200, rendered) |> Conn.halt()
     else
-      _ -> Conn.send_resp(conn, 401, "schema or attributes missing") |> Conn.halt()
+      _ -> conn |> Conn.send_resp(401, "schema or attributes missing") |> Conn.halt()
     end
   end
 
@@ -52,8 +50,9 @@ defmodule E2E.E2EPlug do
   def create(schema, %{} = attrs, count), do: SandboxFactory.insert_list(count, schema, attrs)
 
   @spec atomize(any) :: any
-  defp atomize(%{} = attrs),
-    do: Enum.map(attrs, fn {k, v} -> {String.to_atom(k), atomize(v)} end) |> Enum.into(%{})
+  defp atomize(%{} = attrs) do
+    Enum.map(attrs, fn {k, v} -> {String.to_existing_atom(k), atomize(v)} end) |> Enum.into(%{})
+  end
 
   defp atomize(attrs) when is_list(attrs), do: Enum.map(attrs, &atomize/1)
   defp atomize(v), do: v
